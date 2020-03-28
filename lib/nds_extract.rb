1793a8f2e0a0198a897216cb43e66ab132a6059d
Mert  Ozkaynak <mertozkaynak@windowslive.com>
2020-03-28 21:47:00 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,17 +1,20 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require_relative './directors_database'
-
+require 'pry'
 def directors_totals(source)
   result = {}
   director_index = 0
   while director_index < source.size do
     director = source[director_index]
+  
     result[director[:name]] = gross_for_director(director)
     director_index += 1
+
   end
   result
-end
 
+end
+ 
 def gross_for_director(d)
   total = 0
   index = 0
@@ -25,11 +28,30 @@ def gross_for_director(d)
 end
 
 def list_of_directors(source)
-  # Write this implementation
+  new_array = []
+  director_index = 0 
+  while director_index < source.length do 
+  director = source[director_index][:name]
+  
+  puts new_array << director
+ director_index += 1 
+ end 
+ new_array
 end
 
+#h.each {|key, value| puts "#{key} is #{value}" }
+
 def total_gross(source)
-  # Write this implementation
+  big_total = 0 
+  row_index = 0 
+  while row_index < source.length do
+    directors_name = directors_totals(source)[row_index]
+    big_total += directors_totals(source)[directors_name]
+ end 
+big_total 
+end
+
+# Write this implementation
   #
   # Should use methods:
   # 1. directors_totals: returns a Hash of { dir_name => gross }
@@ -38,6 +60,4 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-end
-
 
