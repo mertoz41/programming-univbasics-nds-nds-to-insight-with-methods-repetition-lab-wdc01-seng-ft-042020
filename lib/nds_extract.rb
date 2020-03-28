84849bbb279c1e00e9c8cd29c35521fb3d644a52
Mert  Ozkaynak <mertozkaynak@windowslive.com>
2020-03-28 21:47:55 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -46,6 +46,7 @@ def total_gross(source)
   row_index = 0 
   while row_index < source.length do
     directors_name = directors_totals(source)[row_index]
+    binding.pry 
     big_total += directors_totals(source)[directors_name]
     binding.pry 
  end 
